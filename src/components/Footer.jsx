b08db1b1e727b3f380eefba5bd0d777600289f17
dapoadedire <adedireadedapo19@gmail.com>
2023-02-17 12:00:44 +0100

    feat: make footer sticky

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-100 p-4 text-center">
+    <footer className="bg-gray-100 p-4 text-center mt-auto">
       <p className="text-sm text-gray-500">
         Made with <span className="text-red-500">&hearts;</span> by{" "}
         <a href="https://twitter.com/dapo_adedire" className="text-blue-500 hover:underline">Dapo Adedire</a>
